a61e4cc8bbdedbabd8ce7af67624f0bcececf8ea
Raul Garcia <raulfgarcia10@gmail.com>
2023-11-07 22:00:47 -0300

    feat: initial layout and axios config

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="bg-gray-100 h-100">
+    <header class="bg-white shadow-md h-16 flex justify-between items-stretch">
+      <router-link :to="{ name: 'home' }"> Home </router-link>
+      <div class="flex items-center gap-1">
+        <router-link
+          to="/"
+          class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100"
+          >Search Meals</router-link
+        >
+        <router-link
+          to="/"
+          class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100"
+          >Meals By Letter</router-link
+        >
+        <router-link
+          to="/"
+          class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100"
+          >Meals By Ingredients</router-link
+        >
+      </div>
+    </header>
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
+//PAREI NO 46:00
+<script setup></script>
+
+<style scoped></style>
